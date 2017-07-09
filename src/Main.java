d51fad4077be7725c70c208b0505804a81c00ccd
tramontaz <Pir0jo4eK>
2017-07-09 10:31:32 +0300

    Add new method for converting array[] to ArrayList. It's will be better.

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        taskOne();
-//        taskTwo();
+//        taskOne();
+        taskTwo();
 //        taskThree();
 
     }
@@ -29,16 +29,14 @@ public class Main {
 
     public static void taskTwo() {
         Integer[] iArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
-        String[] sArray = {"one", "two", "three", "four", "five"};
         Double[] dArray = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 0.0};
-        ArrayList<Integer> iArrayListFromArray = new ArrayOfAnyType<Integer>().arrayToArrayList(iArray);
-        ArrayList<String> sArrayListFromArray = new ArrayOfAnyType<String>().arrayToArrayList(sArray);
-        ArrayList<Double> dArrayListFromArray = new ArrayOfAnyType<Double>().arrayToArrayList(dArray);
-
-        System.out.println(iArrayListFromArray.toString());
-        System.out.println(sArrayListFromArray.toString());
-        System.out.println(dArrayListFromArray.toString());
-
+        String[] sArray = {"one", "two", "three", "four", "five"};
+        arrayToArrayList(iArray);
+        arrayToArrayList(dArray);
+        arrayToArrayList(sArray);
+        System.out.println(Arrays.toString(iArray));
+        System.out.println(Arrays.toString(dArray));
+        System.out.println(Arrays.toString(sArray));
     }
 
     public static void taskThree() {
@@ -68,7 +66,7 @@ public class Main {
 //        }
 //        System.out.println(Arrays.toString(iArray));
 
-    public static void swapTwoNumbersInArray(int[] array, int firstNumberPos, int secondNumberPos) throws WrongPositionException {
+    private static void swapTwoNumbersInArray(int[] array, int firstNumberPos, int secondNumberPos) throws WrongPositionException {
         if (firstNumberPos < 0 || firstNumberPos > array.length ||
                 secondNumberPos < 0 || secondNumberPos > array.length || firstNumberPos == secondNumberPos) {
             throw new WrongPositionException("Неправильно указаны позиции элементов для замены");
@@ -86,5 +84,10 @@ public class Main {
         Object temp = array[firstNumPos];
         array[firstNumPos] = array[secondNumPos];
         array[secondNumPos] = temp;
+    }
+
+    private static <T>ArrayList arrayToArrayList(T[] array){
+        ArrayList<T> arrayList = new ArrayList<>(Arrays.asList(array));
+        return arrayList;
     }
 }
